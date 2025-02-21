c1b650e7a343d2fa202f6fffa31b207344367ff2
Junaid Khan <junaids.khan23@gmail.com>
2025-02-21 16:50:02 +0530

    Product and Category Listing View

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from core.views import index
+from core.views import product_list_view, category_list_view, category_product_list__view, index
 
 app_name = "core"
 
 urlpatterns = [
-    path('',index,name='index')
+    path('',index,name='index'),
+    path('category/',category_list_view, name='category-list'),
+    path('category/<cid>/',category_product_list__view, name='category-product-list'),
+    path('products/',product_list_view, name='product-list'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
+from django.db.models import Count
+from core.models import Category,Products
 
 def index(request):
-    return render(request,'core/index.html')
+    products= Products.objects.filter(product_status="published",featured=True)
+    context = {"products":products}
+    return render(request,'core/index.html',context)
+
+def product_list_view(request):
+    products= Products.objects.filter(product_status="published")
+    context = {"products":products}
+    return render(request,'core/product-list.html',context)
+
+def category_list_view(request):
+    categories= Category.objects.all()
+    context = {"categories":categories}
+    return render(request,'core/category-list.html',context)
+
+def category_product_list__view(request,cid):
+    category = Category.objects.get(cid=cid)
+    products = Products.objects.filter(product_status="published",category=category)
+    context = {
+        "category":category,
+        "products":products
+    }
+    return render(request,'core/category-product-list.html',context)
