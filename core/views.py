d27f49badb41ba7431afc92eb59f84d6c046f708
Junaid Khan <junaids.khan23@gmail.com>
2025-02-22 02:05:13 +0530

    Related Product Changes

--- core/views.py
+++ core/views.py
@@ -26,6 +26,7 @@ def category_product_list__view(request,cid):
 
 def product_detail_view(request,pid):
     product = Products.objects.get(pid=pid)
+    related_products = Products.objects.filter(category=product.category).exclude(pid=pid)
     p_image = product.p_images.all()
-    context = {"p":product,"p_image":p_image}
+    context = {"p":product,"p_image":p_image,"products":related_products}
     return render(request,'core/product-detail.html',context)
